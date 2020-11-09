0273c1d2ff8127b1ec41434d426d11e7a0a27306
Sai Kiran <skiran017@gmail.com>
2020-11-09 17:05:03 +0530

    props and proptypes

--- github-finder/src/App.js
+++ github-finder/src/App.js
@@ -1,16 +1,13 @@
 import React, { Component } from 'react';
+import Navbar from './components/layout/Navbar';
 import './App.css';
 
 class App extends Component {
   render() {
-    const name = 'Kiran';
-    const loading = true;
-
     return (
-      <div className='App'>
-        
-        {loading ? <h4>Loading..</h4> : <h1>Hello from {name}</h1>}
-      </div>
+      <nav className='navbar bg-primary'>
+        <Navbar />
+      </nav>
     );
   }
 }
