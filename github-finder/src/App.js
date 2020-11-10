466481dcd641be16e236317e50b7d7b9014ea07e
Sai Kiran <skiran017@gmail.com>
2020-11-10 23:09:33 +0530

    Not found

--- github-finder/src/App.js
+++ github-finder/src/App.js
@@ -1,18 +1,17 @@
-import React, {  Fragment } from 'react';
+import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Navbar from './components/layout/Navbar';
-import Users from './components/users/Users';
 import User from './components/users/User';
-import Search from './components/users/Search';
 import Alert from './components/layout/Alert';
+import Home from './components/pages/Home';
 import About from './components/pages/About';
+import NotFound from './components/pages/NotFound';
 
 import GithubState from './context/github/GithubState';
 import AlertState from './context/alert/AlertState';
 import './App.css';
 
 const App = () => {
-  
   // async componentDidMount() {
   //   this.setState({ loading: true });
 
@@ -23,9 +22,6 @@ const App = () => {
   //   this.setState({ users: res.data, loading: false });
   // }
 
-  
-  
-
   return (
     <GithubState>
       <AlertState>
@@ -35,18 +31,10 @@ const App = () => {
             <div className='container'>
               <Alert />
               <Switch>
-                <Route
-                  exact
-                  path='/'
-                  render={(props) => (
-                    <Fragment>
-                      <Search  />
-                      <Users />
-                    </Fragment>
-                  )}
-                />
+                <Route exact path='/' component={Home} />
                 <Route exact path='/about' component={About} />
                 <Route exact path='/user/:login' component={User} />
+                <Route component={NotFound} />
               </Switch>
             </div>
           </div>
